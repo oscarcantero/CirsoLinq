1b852f9335914666aa67f09fed3be328247feb03
oscar_cantero@hotmail.com <oscar_cantero@hotmail.com>
2020-05-27 23:43:59 -0500

    Creacion de carpetas de Trabajo

---
+++ Curso Linq/Entidades/Empleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_Linq.Entidades
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
